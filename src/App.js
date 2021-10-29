83988c7861ad1b3db52ca422cfd4b74b5d796a81
slickster23 <tokelo.foso23@gmail.com>
2021-10-29 02:21:59 +0200

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Services from './components/pages/Services';
+import Footer from './components/Footer'
+import Gallery from './components/pages/Gallery';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <Switch>
+      <Route path = '/' exact component = {Home}></Route>
+      <Route path = '/about' exact component = {About}></Route>
+      <Route path = '/services' exact component = {Services}></Route>
+      <Route path = '/blog' exact component = {Home}></Route>
+      <Route path = '/gallery' exact component = {Gallery}></Route>
+    </Switch>
+    </Router>
+    <Footer/>
+    </>
+
   );
 }
 
